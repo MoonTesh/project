e10ae993ee757aaf478f2fa3e5494f86cbf0b7cd
Okneris <n_sirenko2004@ukr.net>
2019-06-20 20:46:26 +0300

    second homework

--- js/main.js
+++ js/main.js
@@ -10,17 +10,66 @@ let appData = {
     savings:false
 };
 
-let expense1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    answer1 = prompt( "Во сколько обойдется?", ''),
-    expense2 = prompt( "Введите обязательную статью расходов в этом месяце", '' ),
-    answer2 = prompt( "Во сколько обойдется?", '');
 
 
-appData.expenses.expense1 = answer1;
-appData.expenses.expense2 = answer2;
 
 
 alert(appData.budget / 30);
 
-let a = 1; let b = 2;
- console.log( a && b ); 
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt( "Во сколько обойдется?", '');
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' ) {
+        console.log ('done');
+        appData.expenses[a] = b;
+    }else{
+        console.log ('error');
+        i--;
+    }
+     
+};
+
+
+// let i=0;
+// while (i<2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt( "Во сколько обойдется?", '');
+//        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '') {
+//         console.log ('done');
+//         appData.expenses[a] = b;
+//        } else {
+//            console.log ('error');
+//            i--;
+//        }
+//     i++;
+// };
+
+
+// let i=0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//     b = prompt( "Во сколько обойдется?", '');
+//     if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '') {
+//         console.log ('done');
+//         appData.expenses[a] = b;
+//        } else {
+//            console.log ('error');
+//            i--;
+//        }
+//     i++;
+// }
+// while (i<2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert ('Бюджет на день составляет: ' + appData.moneyPerDay + 'грн');
+
+if (appData.moneyPerDay < 500) {
+    console.log ('Бедняга!');
+} else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 1000) {
+    console.log ('Терпимо');
+} else if (appData.moneyPerDay > 1000) {
+    console.log ('Хорошо живешь!');
+}
